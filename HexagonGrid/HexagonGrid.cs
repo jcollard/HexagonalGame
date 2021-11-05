b35f7f103c04387ce6694e5a3e559fdf36480b84
Joseph Collard <jcollard@chadwickschool.org>
2021-11-05 04:44:12 -0700

    style: Clean up formatting / layout of HexagonGrid.cs

--- HexagonGrid/HexagonGrid.cs
+++ HexagonGrid/HexagonGrid.cs
@@ -10,24 +10,55 @@
     /// <typeparam name="T">The type of data stored in this HexagonGrid.</typeparam>
     public class HexagonGrid<T>
     {
-
         /// <summary>
         /// The radius of this HexagonGrid.
         /// </summary>
-        public readonly int Radius;
+        private readonly int radius;
 
-        private readonly Dictionary<ValueTuple<int,int>, T> grid;
+        /// <summary>
+        /// A Dictionary tracking the internal values of the Grid.
+        /// </summary>
+        private readonly Dictionary<ValueTuple<int, int>, T> grid;
+
+        /// <summary>
+        /// Initializes an instance of the HexagonGrid class specifying the radius of the grid.
+        /// </summary>
+        /// <param name="radius">The radius of the grid.</param>
+        public HexagonGrid(int radius)
+        {
+            if (radius < 0)
+            {
+                throw new ArgumentException("A HexagonGrid must have a radius of 0 or more.");
+            }
+
+            this.radius = radius;
+            this.grid = new Dictionary<(int, int), T>();
+        }
+
+        /// <summary>
+        /// Gets the radius of this HexagonGrid.
+        /// </summary>
+        public int Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+        }
 
         /// <summary>
         /// A HexagonGrid is indexable.
         /// </summary>
-        /// <value>The x and y coordinate to access.</value>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <value>The element being placed or retrieved.</value>
         public T this[int x, int y]
         {
             get
             {
                 return this.Get(x, y);
             }
+
             set
             {
                 this.Put(x, y, value);
@@ -35,46 +66,48 @@
         }
 
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="radius"></param>
-        public HexagonGrid(int radius)
-        {
-            this.Radius = radius;
-            this.grid = new ();
-        }
-
-        /// <summary>
         /// Checks if the specified coordinates are within the bounds of this
-        /// HexagonGrid. 
+        /// HexagonGrid.
         /// </summary>
         /// <param name="x">The x coordinate.</param>
         /// <param name="y">The y coordinate.</param>
         /// <returns><c>true</c> if the specified coordinates are within the bounds of the HexagonGrid and <c>false</c> otherwise.</returns>
         public bool IsInBounds(int x, int y)
         {
-            return (Math.Abs(x) + Math.Abs(y))/2 <= this.Radius;
+            return (Math.Abs(x) + Math.Abs(y)) / 2 <= this.radius;
         }
 
-        public void Put(int x, int y, T elem)
+        /// <summary>
+        /// Puts the specified element into this HexagonGrid at the specified position.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <param name="elem">The element to put.</param>
+        private void Put(int x, int y, T elem)
         {
             if (!this.IsInBounds(x, y))
             {
-                throw new IndexOutOfRangeException($"The referenced cell ({x}, {y}) is out of bounds for a Grid with radius: {this.Radius}.");
+                throw new IndexOutOfRangeException($"The referenced cell ({x}, {y}) is out of bounds for a Grid with radius: {this.radius}.");
             }
 
-            this.grid[(x,y)] = elem;
+            this.grid[(x, y)] = elem;
         }
 
-        public T Get(int x, int y)
+        /// <summary>
+        /// Gets the element at the specified position.
+        /// </summary>
+        /// <param name="x">The x coordinate.</param>
+        /// <param name="y">The y coordinate.</param>
+        /// <returns>The element at the specified position.</returns>
+        private T Get(int x, int y)
         {
             if (!this.IsInBounds(x, y))
             {
-                throw new IndexOutOfRangeException($"The referenced cell ({x}, {y}) is out of bounds for a Grid with radius: {this.Radius}.");
+                throw new IndexOutOfRangeException($"The referenced cell ({x}, {y}) is out of bounds for a Grid with radius: {this.radius}.");
             }
 
             T value;
-            this.grid.TryGetValue((x,y), out value);
+            this.grid.TryGetValue((x, y), out value);
             return value;
         }
     }
